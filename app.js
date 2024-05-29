64befbc42febe479abe5dacb12ed3bfd11bd516d
CodeWithDaks <daksheshsharma21072000@gmail.com>
2024-05-29 14:12:14 +0530

    file

---
+++ app.js
@@ -0,0 +1,33 @@
+const slides=document.querySelectorAll(".slide");
+
+var counter=0;
+slides.forEach((slide,index)=>{
+     slide.style.left =`${index*100}%`
+});
+
+
+
+
+//Codes for the Buttons
+const goPrev=()=>{
+    counter--
+    slideImage()
+}
+
+const goNext=()=>{
+    counter++
+    slideImage();
+}
+
+
+
+
+
+
+const slideImage=()=>{
+    slides.forEach((slide)=>{
+         slide.style.transform=`translateX(-${counter*100}%)`
+    })
+
+    
+}
